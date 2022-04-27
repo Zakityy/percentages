4627110f9a6633029632ca96d16d554bae4cb238
Zakityy <39856414+Zakityy@users.noreply.github.com>
2022-04-27 02:45:01 -0700

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+var name = prompt("What is your name?");
+if (name == null || name == "") {
+    txt == "No name given";
+} else {
+    txt = "Hello, " + name + "!";
+}
+alert(txt)
+
+alert('My name is bobby');
+var name = prompt('what do you like to do for a living?')
+if (name == null || name == "") {
+    txt == "No answer given";
+} else {
+    txt = "thats so interesting!";
+}
+alert(txt)
+
+alert('Im a browser I cant really do anything :(');
+
+var name = prompt('Are you a only child?')
+if (name == null || name == "") {
+    txt = "No answer given";
+} else {
+    txt = "nice!";
+}
+alert(txt)
