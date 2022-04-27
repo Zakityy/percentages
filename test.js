4022cf8319dec1574c381b22edf022c7f1dc19af
Zakityy <39856414+Zakityy@users.noreply.github.com>
2022-04-26 23:35:20 -0700

    de

--- test.js
+++ test.js
@@ -2,3 +2,9 @@ document.getElementById("button").onclick = function() {
     document.getElementById("para").innerHTML = "virus";
     document.getElementById("button").style.display = "none";
 }
+
+function funcClick() {
+    var x = document.getElementById("gae");
+
+    x.innerHTML = Date();
+}
